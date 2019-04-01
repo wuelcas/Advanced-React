e59a2992e9a684bac5d2b8d394069288f761ddff
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-04-01 14:53:25 -0600

    Finish module 3

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  /* async items(parents, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  } */
+  items: forwardTo('db'),
+};
 
 module.exports = Query;
