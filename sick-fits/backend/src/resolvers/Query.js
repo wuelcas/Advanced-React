01fbc76535e97f5955389c4b7772bdca7d8ec210
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-04 17:04:24 -0600

    27 - logged in user and render props

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,13 +1,23 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  /* async items(parents, args, ctx, info) {
-    const items = await ctx.db.query.items();
-    return items;
-  } */
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+
+    return ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId,
+        },
+      },
+      info,
+    );
+  },
 };
 
 module.exports = Query;
