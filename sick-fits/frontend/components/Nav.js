c7d0bc1e6418978fe3d880b23f02777195cd4a7c
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-05 09:35:15 -0600

    27 - sign in form and custom error handling

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 
 export default function Nav() {
-  return (
-    <NavStyles>
-      <User>
-        {({ data: { me } }) => {
-          if (me) return <p>{me.name}</p>;
-            return null;
-        }}
-      </User>
-      <Link href="/items">
-        <a>Shop</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-      <Link href="/signup">
-        <a>Signup</a>
-      </Link>
-      <Link href="/orders">
-        <a>Orders</a>
-      </Link>
-      <Link href="/me">
-        <a>Account</a>
-      </Link>
-    </NavStyles>
+  return (    
+    <User>
+      {({ data: { me } }) => (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
+          </Link>
+          {me && (
+            <Fragment>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+            </Fragment>
+          )}
+          {!me && (
+            <Fragment>
+              <Link href="/signup">
+                <a>Sign In</a>
+              </Link>
+            </Fragment>
+          )}
+        </NavStyles>
+      )}
+    </User>
   );
 }
