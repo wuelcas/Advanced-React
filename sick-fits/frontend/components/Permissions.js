b97e7a3f9ed5dfe370a25f279fb36f4cf1a2ee70
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-07 16:06:23 -0600

    35 - update permissions on local state

--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,16 +1,17 @@
+import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import Error from './ErrorMessage';
+import Error from "./ErrorMessage";
 import Table from "./styles/Table";
-import SickButton from './styles/SickButton';
+import SickButton from "./styles/SickButton";
 
 const possiblePermissions = [
-  'ADMIN',
-  'USER',
-  'ITEMCREATE',
-  'ITEMUPDATE',
-  'ITEMDELETE',
-  'PERMISSIONUPDATE',
+  "ADMIN",
+  "USER",
+  "ITEMCREATE",
+  "ITEMUPDATE",
+  "ITEMDELETE",
+  "PERMISSIONUPDATE",
 ];
 
 const ALL_USERS_QUERY = gql`
@@ -28,7 +29,7 @@ const Permissions = props => (
   <Query query={ALL_USERS_QUERY}>
     {({ data, loading, error }) => (
       <div>
-        <Error error={error}></Error>
+        <Error error={error} />
         <div>
           <h2>Manage Permissions</h2>
           <Table>
@@ -36,12 +37,16 @@ const Permissions = props => (
               <tr>
                 <th>Name</th>
                 <th>Email</th>
-                {possiblePermissions.map(permission => <th>{permission}</th> )}
+                {possiblePermissions.map(permission => (
+                  <th key={permission}>{permission}</th>
+                ))}
                 <th>👇🏻</th>
               </tr>
             </thead>
             <tbody>
-              {data.users.map(user => <User  user={user} />)}
+              {data.users.map(user => (
+                <UserPermissions key={user.id} user={user} />
+              ))}
             </tbody>
           </Table>
         </div>
@@ -50,7 +55,35 @@ const Permissions = props => (
   </Query>
 );
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired,
+  };
+
+  state = {
+    permissions: this.props.user.permissions,
+  };
+
+  handlePermissionChange = e => {
+    const checkbox = e.target;
+    let updatedPermissions = [...this.state.permissions];
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(
+        permission => permission !== checkbox.value,
+      );
+    }
+    this.setState({
+      permissions: updatedPermissions,
+    });
+  };
+
   render() {
     const user = this.props.user;
 
@@ -59,9 +92,14 @@ class User extends React.Component {
         <td>{user.name}</td>
         <td>{user.email}</td>
         {possiblePermissions.map(permission => (
-          <td>
+          <td key={permission}>
             <label htmlFor={`${user.id}-permission-${permission}`}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                checked={this.state.permissions.includes(permission)}
+                value={permission}
+                onChange={this.handlePermissionChange}
+              />
             </label>
           </td>
         ))}
@@ -69,7 +107,7 @@ class User extends React.Component {
           <SickButton>Update</SickButton>
         </td>
       </tr>
-    )
+    );
   }
 }
 
