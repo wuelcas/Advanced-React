7b8e7caff9393d233c911ab1fa61fb1cebbaa2de
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-11 10:03:01 -0600

    38 - Creating the cart component in react

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -4,16 +4,17 @@ import styled from "styled-components";
 import Nav from "./Nav";
 import Router from "next/router";
 import NProgress from "nprogress";
+import Cart from "./Cart";
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
-}
+};
 Router.onRouteChangeComplete = () => {
   NProgress.done();
-}
+};
 Router.onRouteChangeError = () => {
   NProgress.done();
-}
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -68,7 +69,7 @@ export default function Header() {
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <div>Cart</div>
+      <Cart />
     </StyledHeader>
   );
 }
