82cecd4b9c135f5f2b19fa3d09d84e0d0d9e1d3a
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-14 09:43:13 -0600

    44 - animating our cart component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -5,6 +5,7 @@ import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
 import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 
 export default function Nav() {
   return (
@@ -27,8 +28,11 @@ export default function Nav() {
               </Link>
               <Signout />
               <Mutation mutation={TOGGLE_CART_MUTATION}>
-                {(toggleCart) => (
-                  <button onClick={toggleCart}>My Cart</button>
+                {toggleCart => (
+                  <button onClick={toggleCart}>
+                    My Cart
+                    <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                  </button>
                 )}
               </Mutation>
             </Fragment>
