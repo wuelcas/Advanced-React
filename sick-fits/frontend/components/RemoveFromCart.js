b773151255db0468e3a63a25356c5c0372fe83e0
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-14 08:49:03 -0600

    43 - optimistic response and cache updates

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -28,12 +28,31 @@ class RemoveFromCart extends React.Component {
     id: PropTypes.string.isRequired,
   };
 
+  // This gets called as soon as we get a response back
+  // from the server after a mutation has been performed
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(item => item.id !== cartItemId);
+
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  }
+
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
