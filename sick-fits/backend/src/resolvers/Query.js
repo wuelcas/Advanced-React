da863b30722598445bd7c40db2ac6ceaa26cd7a4
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-17 17:51:25 -0600

    52 - displaying single orders

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -21,13 +21,36 @@ const Query = {
   },
   async users(parent, args, ctx, info) {
     if (!ctx.request.userId) {
-      throw new Error('You must be logged in');
+      throw new Error("You must be logged in");
     }
 
-    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
 
     return ctx.db.query.users({}, info);
-  }
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in");
+    }
+
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info,
+    );
+
+    const ownsOrder = order.userId === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN",
+    );
+
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error("You can't see this bud");
+    }
+
+    return order;
+  },
 };
 
 module.exports = Query;
--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -29,13 +29,17 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
+    NProgress.start();
     const order = await createOrder({
       variables: {
         token: res.id,
       },
     }).catch(err => alert(err.message));
 
-
+    Router.push({
+      pathname: '/order',
+      query: { id: order.data.createOrder.id }
+    });
   };
 
   render() {
