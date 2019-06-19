84e5136ceb3be13fbf4143b267232258053cce41
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-19 10:20:46 -0600

    53 - orders page

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -51,6 +51,18 @@ const Query = {
 
     return order;
   },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error("You must be logged in");
+    }
+
+    return ctx.db.query.orders({
+      where: {
+        user: { id: userId }
+      }
+    }, info);
+  }
 };
 
 module.exports = Query;
