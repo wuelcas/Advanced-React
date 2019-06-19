803bd7e3541a1406f2dedda0786c34618e7202de
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-06-19 17:21:13 -0600

    58 - snapshot testing

---
+++ sick-fits/frontend/__tests__/CartCount.test.js
@@ -0,0 +1,21 @@
+import CartCount from "../components/CartCount";
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+
+describe("<CartCount />", () => {
+  it("renders", () => {
+    shallow(<CartCount count={10} />)
+  });
+  
+  it("matches the snapshot", () => {
+    const wrapper = shallow(<CartCount count={10} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("updates via props", () => {
+    const wrapper = shallow(<CartCount count={10} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+    wrapper.setProps({ count: 40 });
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
--- sick-fits/frontend/__tests__/Item.test.js
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -1,5 +1,6 @@
 import Item from "../components/Item";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
 
 const fakeItem = {
   id: "ABC123",
@@ -7,30 +8,37 @@ const fakeItem = {
   price: 5000,
   description: "Cool item",
   image: "dog.jpg",
-  largeImage: "do-large.jpg"
+  largeImage: "do-large.jpg",
 };
 
 describe("<Item />", () => {
   it("Renders the img properly", () => {
     const wrapper = shallow(<Item item={fakeItem} />);
-    const img = wrapper.find('img');
+    const img = wrapper.find("img");
     expect(img.props().src).toBe(fakeItem.image);
     expect(img.props().alt).toBe(fakeItem.title);
   });
-  
+
   it("Renders the price tag and title properly", () => {
     const wrapper = shallow(<Item item={fakeItem} />);
-    const priceTag = wrapper.find('PriceTag');
+    const priceTag = wrapper.find("PriceTag");
     expect(priceTag.children().text()).toEqual("$50");
-    expect(wrapper.find('Title a').text()).toEqual(fakeItem.title);
+    expect(wrapper.find("Title a").text()).toEqual(fakeItem.title);
   });
 
   it("Renders out the buttons properly", () => {
     const wrapper = shallow(<Item item={fakeItem} />);
-    const buttonList = wrapper.find('.buttonList');
+    const buttonList = wrapper.find(".buttonList");
     expect(buttonList.children()).toHaveLength(3);
-    expect(buttonList.find('Link').exists()).toBe(true);
-    expect(buttonList.find('AddToCart').exists()).toBe(true);
-    expect(buttonList.find('DeleteItem').exists()).toBe(true);
+    expect(buttonList.find("Link").exists()).toBe(true);
+    expect(buttonList.find("AddToCart").exists()).toBe(true);
+    expect(buttonList.find("DeleteItem").exists()).toBe(true);
   });
-})
+});
+
+describe("<Item /> snapshot", () => {
+  it("Renders and matches the snapshot", () => {
+    const wrapper = shallow(<Item item={fakeItem} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+});
